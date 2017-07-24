c3768afa8394281f6b9c2c8fa23c45fc323422ea
Ianis G. Vasilev <ianis@ivasilev.net>
2017-07-24 20:38:58 +0300

    Reformat exercise 1.1

--- Chapter 1/1.rkt
+++ Chapter 1/1.rkt
@@ -10,21 +10,43 @@
 
 (#%require rackunit)
 
-(check-equal? 10 10)
-(check-equal? (+ 5 3 4) 12)
-(check-equal? (- 9 1) 8)
-(check-equal? (/ 6 2) 3)
-(check-equal? (+ (* 2 4) (- 4 6)) 6)
+(check-equal? 10
+              10)
+
+(check-equal? (+ 5 3 4)
+              12)
+
+(check-equal? (- 9 1)
+              8)
+
+(check-equal? (/ 6 2)
+              3)
+
+(check-equal? (+ (* 2 4) (- 4 6))
+              6)
+
 (define a 3) ; Not allowed in expressions
 (define b (+ a 1)) ; Not allowed in expressions
-(check-equal? (+ a b (* a b)) 19)
-(check-equal? (= a b) false)
-(check-equal? (if (and (> b a) (< b (* a b))) b a) 4)
+
+(check-equal? (+ a b (* a b))
+              19)
+
+(check-equal? (= a b)
+              false)
+
+(check-equal? (if (and (> b a) (< b (* a b)))
+                  b
+                  a)
+              4)
+
 (check-equal? (cond ((= a 4) 6)
                     ((= b 4) (+ 6 7 a))
                     (else 25))
               16)
-(check-equal? (+ 2 (if (> b a) b a)) 6)
+
+(check-equal? (+ 2 (if (> b a) b a))
+              6)
+
 (check-equal? (* (cond ((> a b) a)
                        ((< a b) b)
                        (else -1))

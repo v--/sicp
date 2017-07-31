dc339f43a1383f77c1e9d9b96557413ffa9eaa3e
Ianis G. Vasilev <ianis@ivasilev.net>
2017-08-01 02:20:50 +0300

    Finish exercise from 1.3.2

---
+++ chapter-1/0034.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+; Exercise 1.34
+;
+; Suppose we define the procedure
+
+(define (f g)
+  (g 2))
+
+; Then we have
+
+(#%require rackunit)
+
+(define (square x)
+  (* x x))
+
+(check-equal? (f square)
+              4)
+
+(check-equal? (f (lambda (z) (* z (+ z 1))))
+              6)
+
+; What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain.
+
+; Solution
+; The application of (f f) will yield
+;   (f f),
+;   (f 2),
+;   (2 2),
+; and an error will be raised when the interpreter tries to apply 2.

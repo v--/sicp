57f4cf24e62c97fa8feae5256bbd969f07b12256
Ianis G. Vasilev <ianis@ivasilev.net>
2017-08-08 02:12:18 +0300

    Finish exercise from 2.1.1

---
+++ chapter-2/0001.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+; Exercise 2.1
+;
+; Define a better version of make-rat that handles both positive and negative arguments.
+; make-rat should normalize the sign so that if the rational number is positive, both the numerator
+; and denominator are positive, and if the rational number is negative, only the numerator is negative.
+
+; Solution
+
+(define (make-rat n d)
+  (let ([g (gcd n d)]
+        [abs-n (abs n)]
+        [abs-d (abs d)])
+
+    (cons (/ (* n (/ abs-d d)) g)
+          (/ abs-d g))))
+
+(module* test #f
+  (require rackunit)
+
+  (check-equal? (make-rat 10 2)
+                (cons 5 1))
+
+  (check-equal? (make-rat -10 2)
+                (cons -5 1))
+
+  (check-equal? (make-rat 10 -2)
+                (cons -5 1))
+
+  (check-equal? (make-rat -10 -2)
+                (cons 5 1)))

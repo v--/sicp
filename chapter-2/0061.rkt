c3ad8a12701f2e957f2e8c2291e9985e980cc6a3
Ianis G. Vasilev <ianis@ivasilev.net>
2018-07-12 22:41:14 +0300

    Finish exercises from 2.3.3.2

---
+++ chapter-2/0061.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+; Exercise 2.61
+;
+; Give an implementation of adjoin-set using the ordered representation. By analogy with
+; element-of-set? show how to take advantage of the ordering to produce a procedure that requires
+; on the average about half as many steps as with the unordered representation.
+
+; Solution
+
+(define (adjoin-set x set)
+  (let ([smallest (car set)])
+    (cond [(< x smallest) (cons x set)]
+          [(= x smallest) set]
+          [else (cons smallest (adjoin-set x (cdr set)))])))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (adjoin-set 1 '(1 2 3))
+                '(1 2 3))
+
+  (check-equal? (adjoin-set 1 '(2 3))
+                '(1 2 3))
+
+  (check-equal? (adjoin-set 2 '(1 3))
+                '(1 2 3)))
---
+++ chapter-2/0062.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+; Exercise 2.62
+;
+; Give a Theta(n) implementation of union-set for sets represented as ordered lists.
+
+; Solution
+
+(define (union-set set1 set2)
+  (cond [(null? set1) set2]
+        [(null? set2) set1]
+        [(> (car set1) (car set2)) (union-set set2 set1)]
+        [(= (car set1) (car set2)) (cons (car set1)
+                                         (union-set (cdr set1) (cdr set2)))]
+        [else (cons (car set1)
+                    (union-set (cdr set1) set2))]
+        ))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (union-set null '(1 2 3 4))
+                '(1 2 3 4))
+
+  (check-equal? (union-set '(1 2) '(1 2 3 4))
+                '(1 2 3 4))
+
+  (check-equal? (union-set '(1 2) '(3 4))
+                '(1 2 3 4))
+
+  (check-equal? (union-set '(2 3) '(1 4))
+                '(1 2 3 4)))

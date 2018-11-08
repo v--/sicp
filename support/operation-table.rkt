c0d7c566685b08bca0acacc785f8719e754e8a1f
Ianis Vasilev <ianis@ivasilev.net>
2018-11-08 20:14:32 +0200

    Revert the "coerced" apply-generic operation

--- support/operation-table.rkt
+++ support/operation-table.rkt
@@ -36,21 +36,8 @@
     (let ([proc (get op type-tags)])
       (if proc
           (apply proc (map contents args))
-          (if (= (length args) 2)
-              (let ([type1 (car type-tags)]
-                    [type2 (cadr type-tags)]
-                    [a1 (car args)]
-                    [a2 (cadr args)])
-                (let ([t1->t2 (get-coercion type1 type2)]
-                      [t2->t1 (get-coercion type2 type1)])
-                  (cond [t1->t2
-                         (apply-generic op (t1->t2 a1) a2)]
-                        [t2->t1
-                         (apply-generic op a1 (t2->t1 a2))]
-                        [else
-                         (error "No method for these types"
-                                (list op type-tags))])))
-              (error "No method for these types"
-                     (list op type-tags)))))))
+          (error
+            "No method for these types -- APPLY-GENERIC"
+            (list op type-tags))))))
 
 (provide put get put-coercion get-coercion attach-tag type-tag contents apply-generic)
